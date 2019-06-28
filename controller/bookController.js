9f650ce1dd3f877906f59c626022fe3d67c0da0b
alex <bezzil85@gmail.com>
2019-06-28 21:30:09 +0300

    контроллер книги  - удаление  - апдейт  - выборка с сортировкой, групировкой, лимитами и офсетом
     картинка теперь не обязательна в форме как на апдейт так и на создание записи

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,4 +1,6 @@
 const express = require('express')
+  , fs = require('fs')
+  , path = require('path')
   , router = express.Router()
   , Book = require('../model/book')
   , multer  = require('multer')
@@ -7,21 +9,66 @@ const express = require('express')
 			cb(null, 'uploads/books/')
 		},
 		filename: function (req, file, cb) {
-			cb(null, file.originalname); //Не безопасно
+			cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); //Не безопасно
 		}
 	})
-  , upload = multer({ storage: storage });
+  , upload = multer({
+		storage: storage
+  	}).any();
 
 
-router.post('/book/create', upload.array('image'),  async (req, res) => {
+router.post('/book/create', upload,  async (req, res) => {
 
-	if (await Book.create(Object.assign(req.body, {image: req.files[0].filename}))){
-		res.send('record created')
+	if (await Book.create(Object.assign(req.body, {
+		image: (req.files && req.files.length ? req.files[0].filename : false)
+	}))){
+		res.json({'status': 'record created'});
 	}else{
-		res.send('error saving book')
+		res.json({'status': 'error saving record'});
 	}
 
 });
+
+router.post('/book/update/:id', upload, async (req, res) => {
+
+	if (await Book.update(Object.assign(req.body, {
+		  image: (req.files && req.files.length ? req.files[0].filename : false)
+	  }), req.params.id)){
+		res.json({'status': 'record updated'});
+	}else{
+		res.json({'status': 'error updating record'});
+	}
+
+});
+
+
+router.get('/book/delete/:id', async (req, res) => {
+
+	if (await Book.delete(req.params.id)){
+		res.json({'status': 'record deleted'});
+	}else{
+		res.json({'status': 'error deleting record'});
+	}
+
+});
+
+
+//http://localhost:3001/book/fetch?limit=5&offset=20&orderby=id_DESC&groupby=author
+//http://localhost:3001/book/fetch?limit=25
+//http://localhost:3001/book/fetch?offset=100
+//http://localhost:3001/book/fetch?orderby=date_DESC
+
+router.get('/book/fetch', async (req, res) => {
+
+	let books = await Book.fetch(req.query);
+	if (books){
+		res.json(books);
+	}else{
+		res.json([]);
+	}
+
+});
+
 
 router.get('/book/count', async (req, res) => {
 
--- model/book.js
+++ model/book.js
@@ -3,13 +3,13 @@ const db = require('../db');
 exports.create = async (book) => {
 	try {
 		await db.query(`
-			INSERT INTO books (title, date, author, description, image) 
+			INSERT INTO books (title, date, author, description${ book.image ? ', image' : '' }) 
 			VALUES (
 				"${ book.title }",
 				"${ book.date }",
 				"${ book.author }",
-				"${ book.description }",
-				"${ book.image }"
+				"${ book.description }"
+				${ book.image ? `, "${book.image}"` : "" }
 				)
 		`);
 		return true;
@@ -21,8 +21,50 @@ exports.create = async (book) => {
 
 };
 
+
+exports.update = async (book, id)=>{
+	try {
+		await  db.query(`UPDATE books SET ${ Object.entries(book).map((n)=>{ 
+			return `${n[0]} = "${n[1]}"`
+		}).join(", ") } WHERE id=${id}`);
+		return true;
+	} catch (e){
+		console.log(e);
+		return false;
+
+	}
+};
+
+
+exports.delete = async (id) => {
+	try {
+		await  db.query(`DELETE FROM books WHERE id=${id}`);
+		return true;
+	} catch (e){
+		console.log(e);
+		return false;
+	}
+};
+
+exports.fetch = async (options) => {
+	try {
+		console.log(options);
+		let query = `SELECT * FROM books 
+		${ options.groupby ? "GROUP BY " + options.groupby : ""}
+		${ options.orderby ? "ORDER BY " + options.orderby.split("_").join(" ") : ""}
+		${ options.limit ? "LIMIT " + options.limit : "LIMIT 10"} 
+		${ options.offset ? "OFFSET " + options.offset : ""}
+		`;
+
+		let [rows, fields] = await db.query(query);
+		return rows;
+	} catch (e) {
+		console.log(e);
+		return false;
+	}
+};
+
 exports.count = async () => {
 	const [rows, fields] = await db.query(`SELECT COUNT(*) AS booksCount FROM books LIMIT 1;`);
 	return rows[0].booksCount;
 };
-
